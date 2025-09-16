87f7f83bd82d054c9ecb918632f55fcce7f0b1fc
Shivansh Mishra <shivanshmishra151@gmail.com>
2025-09-16 23:46:56 +0530

    Initialization

---
+++ models/Contact.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose')
+// models/Contact.js
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true, // Each email should be unique
+        trim: true
+    },
+    company: {
+        type: String,
+        required: [true, 'Please provide a company name'],
+        trim: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Contact', ContactSchema);
